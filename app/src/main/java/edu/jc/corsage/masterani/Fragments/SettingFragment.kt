454a9701db957470e77d42cefedbc85400600dd5
Corsage <j3chowdh@edu.uwaterloo.ca>
2017-10-02 23:45:08 -0400

    Created basic settings page.

--- app/src/main/java/edu/jc/corsage/masterani/Fragments/SettingFragment.kt
+++ app/src/main/java/edu/jc/corsage/masterani/Fragments/SettingFragment.kt
@@ -1,20 +1,15 @@
 package edu.jc.corsage.masterani.Fragments
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.preference.PreferenceFragmentCompat
 import edu.jc.corsage.masterani.R
 
 /**
  * Created by j3chowdh on 10/1/2017.
  */
 
-class SettingFragment: Fragment() {
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.view_setting, container, false)
-
-        return view
+class SettingFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
     }
 }
