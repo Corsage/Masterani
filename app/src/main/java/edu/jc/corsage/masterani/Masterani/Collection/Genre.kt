f5e68e9fdca1a703f6e4d5129070816b45f51b10
Corsage <j3chowdh@edu.uwaterloo.ca>
2017-10-23 23:20:20 -0400

    Added genre ids for sorting.

---
+++ app/src/main/java/edu/jc/corsage/masterani/Masterani/Collection/Genre.kt
@@ -0,0 +1,50 @@
+package edu.jc.corsage.masterani.Masterani.Collection
+
+/**
+ * Created by j3chowdh on 10/23/2017.
+ */
+
+enum class Genre(private val id: Int) {
+    ACTION(57),
+    COMEDY(59),
+    DRAMA(60),
+    GAME(93),
+    HORROR(71),
+    MAGIC(88),
+    MILITARY(83),
+    PARODY(94),
+    ROMANCE(67),
+    SCI_FI(61),
+    SHOULJO_AI(92),
+    SLICE_OF_LIFE(68),
+    SUPER_POWER(76),
+    VAMPIRE(81),
+    ADVENTURE(58),
+    DEMENTIA(84),
+    ECCHI(79),
+    HAREM(89),
+    JOSEI(66),
+    MARTIAL_ARTS(75),
+    MUSIC(90),
+    POLICE(72),
+    SAMURAI(87),
+    SEINEN(70),
+    SHOUNEN(64),
+    SPACE(62),
+    SUPERNATURAL(80),
+    YAOI(98),
+    CARS(69),
+    DEMONS(86),
+    FANTASY(77),
+    HISTORICAL(82),
+    KIDS(95),
+    MECHA(85),
+    MYSTERY(63),
+    PSYCHOLOGICAL(73),
+    SCHOOL(78),
+    SHOUJO(91),
+    SHOUNEN_AI(96),
+    SPORTS(65),
+    THRILLER(74),
+    YURI(97)
+}
